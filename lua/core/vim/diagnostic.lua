281c3c857dd9989a757aec8c3af44c32bfdc93eb
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-22 23:31:51 -0300

    refactor: change diagnostic config

--- lua/core/vim/diagnostic.lua
+++ lua/core/vim/diagnostic.lua
@@ -1,24 +1,10 @@
-vim.diagnostic.config({
-	signs = true,
-	underline = true,
-	severity_sort = true,
-	update_in_insert = false,
-	virtual_text = {
-		spacing = 2,
-		prefix = "●",
-		severity = { min = vim.diagnostic.severity.WARN },
-	},
-	float = {
-		border = "rounded",
-		source = "if_many",
-		focusable = false,
-		style = "minimal",
-	},
+vim.api.nvim_create_autocmd("CursorHold", {
+	callback = function()
+		vim.diagnostic.open_float({
+			border = "rounded",
+			source = true,
+			focusable = true,
+			style = "minimal",
+		})
+	end,
 })
-
-for type, icon in pairs({ Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
-vim.api.nvim_create_autocmd("CursorHold", { callback = vim.diagnostic.open_float })
