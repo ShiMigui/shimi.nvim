eba439f6bcf82076c093dc50586dae8ec69a70bf
Miguel Nascimento <miguel.nasto@gmail.com>
2026-06-05 16:08:08 -0300

    refactor: use built-in auto_install and opts pattern for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,27 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"jwalton512/vim-blade",
+	},
 	build = ":TSUpdate",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"markdown",
+			"markdown_inline",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
+	},
 	config = function(_, opts)
-		local ts = require("nvim-treesitter")
-		ts.setup(opts)
-
-		local install = require("nvim-treesitter.install")
-		local installed = {}
-		for i, v in ipairs(ts.get_installed()) do
-			installed[v] = i
-		end
-
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function(args)
-				local buf = args.buf
-
-				if vim.bo[buf].buftype ~= "" or vim.api.nvim_buf_get_name(buf) == "" then
-					return
-				end
-
-				local ft = vim.treesitter.language.get_lang(args.match) or args.match
-				if installed[ft] then
-					return
-				end
-				install.install({ ft })
-				installed[ft] = true
-			end,
-		})
+		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
