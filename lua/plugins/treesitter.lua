daeda2ad5777077bab88d5b420e92fadd57d6469
Miguel Nascimento <miguel.nasto@gmail.com>
2026-06-05 16:08:24 -0300

    feat: add EmmyLua docs and incremental selection to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,11 @@
+---@type LazySpec
 return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = {
 		"jwalton512/vim-blade",
 	},
 	build = ":TSUpdate",
+	---@type TSConfig
 	opts = {
 		ensure_installed = {
 			"lua",
@@ -14,13 +16,26 @@ return {
 			"markdown_inline",
 		},
 		auto_install = true,
+		sync_install = false,
 		highlight = {
 			enable = true,
+			additional_vim_regex_highlighting = false,
 		},
 		indent = {
 			enable = true,
 		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
 	},
+	---@param _ any
+	---@param opts TSConfig
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
 	end,
