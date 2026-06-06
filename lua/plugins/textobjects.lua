626b88769486bbf35ae79acbbe4f1d7deb102ccc
Miguel Nascimento <miguel.nasto@gmail.com>
2026-06-05 22:34:19 -0300

    refactor: compact textobjects plugin with loops and doc header
    - Reduce 213 lines to 79 by generating select/move keymaps via for loops
    - Add concise documentation header explaining all keymap groups
    - Use helper closures to avoid repetitive anonymous functions

--- lua/plugins/textobjects.lua
+++ lua/plugins/textobjects.lua
@@ -1,4 +1,25 @@
+--[[
+nvim-treesitter-textobjects keymaps
+
+Select (x/o): a{m,c,p,i,b,/} outer · i{m,c,p,i,b,/} inner · as scope
+Swap (n):     <leader>sn next param · <leader>sp prev param
+Move (n/x/o): ]m/[m function · ]]/[[ class · ]M/[M function end · ][/[] class end · ]s/[s scope
+Repeat:       ; forward · , backward
+]]
+
 local keymaps = require("config.keymaps").keymaps
+
+local function select_obj(capture, query)
+	return function()
+		require("nvim-treesitter-textobjects.select").select_textobject("@" .. capture, query)
+	end
+end
+
+local function move_obj(fn, capture, query)
+	return function()
+		fn("@" .. capture, query)
+	end
+end
 
 return {
 	"nvim-treesitter/nvim-treesitter-textobjects",
@@ -14,100 +35,44 @@ return {
 				},
 				include_surrounding_whitespace = false,
 			},
-			move = {
-				set_jumps = true,
-			},
+			move = { set_jumps = true },
 		})
 
-		local select = require("nvim-treesitter-textobjects.select")
-		local swap = require("nvim-treesitter-textobjects.swap")
-		local move = require("nvim-treesitter-textobjects.move")
-		local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
+		for _, obj in ipairs({
+			{ "m", "function" },
+			{ "c", "class" },
+			{ "p", "parameter" },
+			{ "i", "conditional" },
+			{ "b", "block" },
+			{ "/", "comment" },
+		}) do
+			local key, name = obj[1], obj[2]
+			keymaps["Select " .. name .. " outer"] = { modes = { "x", "o" }, lhs = "a" .. key, rhs = select_obj(name .. ".outer", "textobjects") }
+			keymaps["Select " .. name .. " inner"] = { modes = { "x", "o" }, lhs = "i" .. key, rhs = select_obj(name .. ".inner", "textobjects") }
+		end
+		keymaps["Select scope"] = { modes = { "x", "o" }, lhs = "as", rhs = select_obj("local.scope", "locals") }
 
-		-- Select text objects
-		keymaps["Select function outer"] = { modes = { "x", "o" }, lhs = "am", rhs = function()
-			select.select_textobject("@function.outer", "textobjects")
-		end }
-		keymaps["Select function inner"] = { modes = { "x", "o" }, lhs = "im", rhs = function()
-			select.select_textobject("@function.inner", "textobjects")
-		end }
-		keymaps["Select class outer"] = { modes = { "x", "o" }, lhs = "ac", rhs = function()
-			select.select_textobject("@class.outer", "textobjects")
-		end }
-		keymaps["Select class inner"] = { modes = { "x", "o" }, lhs = "ic", rhs = function()
-			select.select_textobject("@class.inner", "textobjects")
-		end }
-		keymaps["Select parameter outer"] = { modes = { "x", "o" }, lhs = "ap", rhs = function()
-			select.select_textobject("@parameter.outer", "textobjects")
-		end }
-		keymaps["Select parameter inner"] = { modes = { "x", "o" }, lhs = "ip", rhs = function()
-			select.select_textobject("@parameter.inner", "textobjects")
-		end }
-		keymaps["Select conditional outer"] = { modes = { "x", "o" }, lhs = "ai", rhs = function()
-			select.select_textobject("@conditional.outer", "textobjects")
-		end }
-		keymaps["Select conditional inner"] = { modes = { "x", "o" }, lhs = "ii", rhs = function()
-			select.select_textobject("@conditional.inner", "textobjects")
-		end }
-		keymaps["Select block outer"] = { modes = { "x", "o" }, lhs = "ab", rhs = function()
-			select.select_textobject("@block.outer", "textobjects")
-		end }
-		keymaps["Select block inner"] = { modes = { "x", "o" }, lhs = "ib", rhs = function()
-			select.select_textobject("@block.inner", "textobjects")
-		end }
-		keymaps["Select comment outer"] = { modes = { "x", "o" }, lhs = "a/", rhs = function()
-			select.select_textobject("@comment.outer", "textobjects")
-		end }
-		keymaps["Select comment inner"] = { modes = { "x", "o" }, lhs = "i/", rhs = function()
-			select.select_textobject("@comment.inner", "textobjects")
-		end }
-		keymaps["Select scope"] = { modes = { "x", "o" }, lhs = "as", rhs = function()
-			select.select_textobject("@local.scope", "locals")
-		end }
+		for _, obj in ipairs({
+			{ next = "]m",  prev = "[m",  name = "function", query = "textobjects", dir = "start" },
+			{ next = "]]",  prev = "[[",  name = "class",    query = "textobjects", dir = "start" },
+			{ next = "]M",  prev = "[M",  name = "function", query = "textobjects", dir = "end" },
+			{ next = "][",  prev = "[]",  name = "class",    query = "textobjects", dir = "end" },
+			{ next = "]s",  prev = "[s",  name = "scope",    query = "locals",     dir = "start" },
+		}) do
+			local fn = obj.dir == "start" and require("nvim-treesitter-textobjects.move").goto_next_start or require("nvim-treesitter-textobjects.move").goto_next_end
+			local fp = obj.dir == "start" and require("nvim-treesitter-textobjects.move").goto_previous_start or require("nvim-treesitter-textobjects.move").goto_previous_end
+			keymaps["Move next " .. obj.name] = { modes = { "n", "x", "o" }, lhs = obj.next, rhs = move_obj(fn, obj.name .. ".outer", obj.query) }
+			keymaps["Move prev " .. obj.name] = { modes = { "n", "x", "o" }, lhs = obj.prev, rhs = move_obj(fp, obj.name .. ".outer", obj.query) }
+		end
 
-		-- Swap
 		keymaps["Swap next parameter"] = { leader = "sn", rhs = function()
-			swap.swap_next("@parameter.inner")
+			require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
 		end }
 		keymaps["Swap prev parameter"] = { leader = "sp", rhs = function()
-			swap.swap_previous("@parameter.outer")
+			require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.outer")
 		end }
 
-		-- Move: next
-		keymaps["Next function start"] = { modes = { "n", "x", "o" }, lhs = "]m", rhs = function()
-			move.goto_next_start("@function.outer", "textobjects")
-		end }
-		keymaps["Next class start"] = { modes = { "n", "x", "o" }, lhs = "]]", rhs = function()
-			move.goto_next_start("@class.outer", "textobjects")
-		end }
-		keymaps["Next function end"] = { modes = { "n", "x", "o" }, lhs = "]M", rhs = function()
-			move.goto_next_end("@function.outer", "textobjects")
-		end }
-		keymaps["Next class end"] = { modes = { "n", "x", "o" }, lhs = "][", rhs = function()
-			move.goto_next_end("@class.outer", "textobjects")
-		end }
-		keymaps["Next scope"] = { modes = { "n", "x", "o" }, lhs = "]s", rhs = function()
-			move.goto_next_start("@local.scope", "locals")
-		end }
-
-		-- Move: previous
-		keymaps["Prev function start"] = { modes = { "n", "x", "o" }, lhs = "[m", rhs = function()
-			move.goto_previous_start("@function.outer", "textobjects")
-		end }
-		keymaps["Prev class start"] = { modes = { "n", "x", "o" }, lhs = "[[", rhs = function()
-			move.goto_previous_start("@class.outer", "textobjects")
-		end }
-		keymaps["Prev function end"] = { modes = { "n", "x", "o" }, lhs = "[M", rhs = function()
-			move.goto_previous_end("@function.outer", "textobjects")
-		end }
-		keymaps["Prev class end"] = { modes = { "n", "x", "o" }, lhs = "[]", rhs = function()
-			move.goto_previous_end("@class.outer", "textobjects")
-		end }
-		keymaps["Prev scope"] = { modes = { "n", "x", "o" }, lhs = "[s", rhs = function()
-			move.goto_previous_start("@local.scope", "locals")
-		end }
-
-		-- Repeatable movements
+		local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
 		keymaps["Repeat last move next"] = { modes = { "n", "x", "o" }, lhs = ";", rhs = ts_repeat_move.repeat_last_move_next }
 		keymaps["Repeat last move prev"] = { modes = { "n", "x", "o" }, lhs = ",", rhs = ts_repeat_move.repeat_last_move_previous }
 	end,
