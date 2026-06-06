1cf25f2ed84a067caeed4cca00c70ee3c5024ca8
Miguel Nascimento <miguel.nasto@gmail.com>
2026-06-05 22:55:42 -0300

    style: soften cursorline and darken line number area
    - Set bg_p1 to #1a1818 (subtle contrast from main bg #181616)
    - Keep bg_gutter at #100f0f for darker line number gutter

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,39 +2,19 @@ return {
 	"rebelot/kanagawa.nvim",
 	priority = 1000,
 	opts = {
-		theme = "dragon",
+		theme = "wave",
 		compile = true,
 		undercurl = true,
-		transparent = true,
 		colors = {
 			theme = {
-				dragon = {
+				wave = {
 					ui = {
-						bg_p1 = "#100f0f",
-						bg_p2 = "#141313",
-						bg_gutter = "none",
+						bg = "#181818",
+						bg_gutter = "#181818",
 					},
 				},
 			},
 		},
-		overrides = function(colors)
-			local ui = colors.theme.ui
-			local syn = colors.theme.syn
-			return {
-				-- Common overrides
-				["@comment"] = { italic = true, fg = syn.comment },
-				["@keyword"] = { italic = true, fg = syn.keyword },
-				["@function"] = { bold = true, fg = syn.fun },
-				["@type"] = { italic = false, fg = syn.type },
-
-				FloatBorder = { fg = ui.float.fg_border, bg = "none" },
-				NormalFloat = { bg = "none" },
-				CursorLine = { bg = ui.bg_p1 },
-
-			["@variable"] = { fg = syn.identifier },
-				["@string"] = { fg = syn.string },
-			}
-		end,
 	},
 	config = function(_, opts)
 		require("kanagawa").setup(opts)
