605c1ab0c65b6dd2606f3c52fb26e75a78c5ffbf
Miguel Nascimento <miguel.nasto@gmail.com>
2026-06-05 22:47:44 -0300

    style: darken theme and make syntax text stand out
    - Remove transparency for a solid dark background
    - Add bold to keywords, functions, types, variables, constants,
      strings, operators, properties, conditionals, and more
    - Polish UI highlights: Visual, LineNr, Search, cursor line

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -5,7 +5,6 @@ return {
 		theme = "dragon",
 		compile = true,
 		undercurl = true,
-		transparent = true,
 		colors = {
 			theme = {
 				all = {
@@ -19,19 +18,41 @@ return {
 			local ui = colors.theme.ui
 			local syn = colors.theme.syn
 			return {
-				-- Common overrides
+				-- Make text stand out
 				["@comment"] = { italic = true, fg = syn.comment },
-				["@keyword"] = { italic = true, fg = syn.keyword },
+				["@keyword"] = { bold = true, italic = true, fg = syn.keyword },
+				["@keyword.function"] = { bold = true, italic = true, fg = syn.keyword },
+				["@keyword.return"] = { bold = true, fg = syn.keyword },
 				["@function"] = { bold = true, fg = syn.fun },
-				["@type"] = { italic = false, fg = syn.type },
+				["@function.builtin"] = { bold = true, fg = syn.fun },
+				["@type"] = { bold = true, fg = syn.type },
+				["@type.builtin"] = { bold = true, fg = syn.type },
+				["@variable"] = { bold = true, fg = syn.identifier },
+				["@variable.builtin"] = { bold = true, fg = syn.identifier },
+				["@constant"] = { bold = true, fg = syn.constant },
+				["@number"] = { bold = true, fg = syn.number },
+				["@boolean"] = { bold = true, fg = syn.boolean },
+				["@string"] = { bold = true, fg = syn.string },
+				["@string.special"] = { bold = true, fg = syn.string },
+				["@operator"] = { bold = true, fg = syn.operator },
+				["@property"] = { bold = true, fg = syn.identifier },
+				["@parameter"] = { italic = true, fg = syn.parameter },
+				["@include"] = { bold = true, fg = syn.keyword },
+				["@conditional"] = { bold = true, fg = syn.keyword },
+				["@repeat"] = { bold = true, fg = syn.keyword },
+				["@exception"] = { bold = true, fg = syn.keyword },
+				["@punctuation"] = { fg = syn.operator },
+				["@namespace"] = { bold = true, fg = syn.namespace },
 
+				-- Clean UI
 				FloatBorder = { fg = ui.float.fg_border, bg = "none" },
 				NormalFloat = { bg = "none" },
 				CursorLine = { bg = ui.bg_p1 },
-
-				-- Make the light theme (Lotus) look better
-				["@variable"] = { fg = syn.identifier },
-				["@string"] = { fg = syn.string },
+				Visual = { bg = ui.bg_p2 },
+				LineNr = { fg = ui.bg_p2 },
+				CursorLineNr = { bold = true, fg = syn.fun },
+				Search = { bg = ui.bg_p2, fg = syn.keyword },
+				IncSearch = { bg = ui.bg_p2, fg = syn.fun },
 			}
 		end,
 	},
