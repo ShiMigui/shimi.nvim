845da5bc455d4b7d52c890f03ac1bb6eeba50857
ShiMigui <miguel.nasto@gmail.com>
2026-08-31 14:19:36 -0300

    feat: open mini.files on startup instead of netrw
    Disable netrw and open mini.files automatically on VimEnter when no file
    or directory argument is given, so nvim boots directly into the explorer
    instead of the netrw welcome screen.

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 _G.THEME = ({
 	DEFAULT = "onedark",
 	LIGHT = "onelight",
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,3 +17,14 @@ vim.diagnostic.config({
 })
 
 api.nvim_create_autocmd("CursorHold", { callback = vim.diagnostic.open_float })
+
+api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if not vim.tbl_isempty(vim.fn.argv()) then
+			return
+		end
+
+		require("lazy").load({ plugins = { "mini.nvim" } })
+		require("mini.files").open()
+	end,
+})
